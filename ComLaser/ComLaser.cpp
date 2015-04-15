3e612fd60fff5e9c0ff2b336121a1dbf3dccd6bd
cljaejung <cljaejung@gmail.com>
2015-04-15 15:58:47 +0900

    update comlaser window env

--- ComLaser/ComLaser.cpp
+++ ComLaser/ComLaser.cpp
@@ -18,6 +18,7 @@
 // ----------------------------------------------------------------------------
  
 // For compilers that support precompilation, includes "wx/wx.h".
+#include "stdafx.h"
 #include "wx/wxprec.h"
  
 #ifdef __BORLANDC__
