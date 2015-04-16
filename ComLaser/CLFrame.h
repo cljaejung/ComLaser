4b47d484c2191d06b3e0f2957293ea0f08d025d0
cljaejung <cljaejung@gmail.com>
2015-04-16 20:51:36 +0900

    update

--- ComLaser/CLFrame.h
+++ ComLaser/CLFrame.h
@@ -10,27 +10,27 @@
 #include "wx/wx.h"
 #endif
 
-//
-//class cMainPanel;
-//class cOperatorPanel;
-//class cCLFrame : public wxFrame
-//{
-//public:
-//	// ctor(s)
-//	cCLFrame(const wxString& title);
-//
-//	// event handlers (these functions should _not_ be virtual)
-//	void OnQuit(wxCommandEvent& event);
-//	void OnAbout(wxCommandEvent& event);
-//
-//
-//public:
-////	cMainPanel *m_mainPanel;
-////	cOperatorPanel *m_loginPanel;
-//
-//
-//private:
-//	// any class wishing to process wxWidgets events must use this macro
-//	DECLARE_EVENT_TABLE()
-//};
-//
+
+class cMainPanel;
+class cOperatorPanel;
+class cCLFrame : public wxFrame
+{
+public:
+	// ctor(s)
+	cCLFrame(const wxString& title);
+
+	// event handlers (these functions should _not_ be virtual)
+	void OnQuit(wxCommandEvent& event);
+	void OnAbout(wxCommandEvent& event);
+
+
+public:
+//	cMainPanel *m_mainPanel;
+//	cOperatorPanel *m_loginPanel;
+
+
+private:
+	// any class wishing to process wxWidgets events must use this macro
+	DECLARE_EVENT_TABLE()
+};
+
