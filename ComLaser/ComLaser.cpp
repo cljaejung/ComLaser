15906051851d61f65c670acc8b3a2ca2100abbce
cljaejung <cljaejung@gmail.com>
2015-04-16 22:09:41 +0900

    update

--- ComLaser/ComLaser.cpp
+++ ComLaser/ComLaser.cpp
@@ -12,6 +12,27 @@ IMPLEMENT_APP(cCLApp)
 
 
 
+class cMainPanel : public wxPanel
+{
+public:
+	cMainPanel(wxFrame*frame);
+	virtual ~cMainPanel();
+
+
+protected:
+
+
+public:
+
+
+	// message handling
+protected:
+	DECLARE_EVENT_TABLE()
+	void OnButtonOperator(wxCommandEvent &);
+};
+
+
+
 // 'Main program' equivalent: the program execution "starts" here
 bool cCLApp::OnInit()
 {
@@ -24,3 +45,83 @@ bool cCLApp::OnInit()
 }
 
 
+
+
+
+enum {
+	ID_PANEL,
+	ID_BUTTON,
+	ID_BUTTON1,
+	ID_BUTTON2,
+	ID_BUTTON3,
+	ID_BUTTON4,
+	ID_BUTTON5,
+	ID_BUTTON_OPERATOR,
+};
+
+
+wxBEGIN_EVENT_TABLE(cMainPanel, wxPanel)
+EVT_BUTTON(ID_BUTTON_OPERATOR, cMainPanel::OnButtonOperator)
+wxEND_EVENT_TABLE()
+
+cMainPanel::cMainPanel(wxFrame*frame) :
+wxPanel(frame)
+{
+
+	cMainPanel* itemPanel1 = this;
+
+	wxBoxSizer* itemBoxSizer2 = new wxBoxSizer(wxVERTICAL);
+	itemPanel1->SetSizer(itemBoxSizer2);
+
+	wxBoxSizer* itemBoxSizer3 = new wxBoxSizer(wxHORIZONTAL);
+	itemBoxSizer2->Add(itemBoxSizer3, 1, wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
+
+	wxStaticText* itemStaticText4 = new wxStaticText(itemPanel1, wxID_STATIC, _("Static text"), wxDefaultPosition, wxDefaultSize, 0);
+	itemBoxSizer3->Add(itemStaticText4, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+	wxBoxSizer* itemBoxSizer5 = new wxBoxSizer(wxHORIZONTAL);
+	itemBoxSizer2->Add(itemBoxSizer5, 1, wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
+
+	wxButton* itemButton6 = new wxButton(itemPanel1, ID_BUTTON, _("Enforcement\nSetting"), wxDefaultPosition, wxDefaultSize, 0);
+	itemBoxSizer5->Add(itemButton6, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+	wxButton* itemButton7 = new wxButton(itemPanel1, ID_BUTTON1, _("Camera\nSetting"), wxDefaultPosition, wxDefaultSize, 0);
+	itemBoxSizer5->Add(itemButton7, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+	wxButton* itemButton8 = new wxButton(itemPanel1, ID_BUTTON_OPERATOR, _("Operator\nSetting"), wxDefaultPosition, wxDefaultSize, 0);
+	itemBoxSizer5->Add(itemButton8, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+	wxButton* itemButton9 = new wxButton(itemPanel1, ID_BUTTON3, _("File \nManagement"), wxDefaultPosition, wxDefaultSize, 0);
+	itemBoxSizer5->Add(itemButton9, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+	wxButton* itemButton10 = new wxButton(itemPanel1, ID_BUTTON4, _("Enforcement"), wxDefaultPosition, wxDefaultSize, 0);
+	itemBoxSizer5->Add(itemButton10, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+	wxBoxSizer* itemBoxSizer11 = new wxBoxSizer(wxHORIZONTAL);
+	itemBoxSizer2->Add(itemBoxSizer11, 1, wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
+
+	wxStaticText* itemStaticText12 = new wxStaticText(itemPanel1, wxID_STATIC, _("Static text"), wxDefaultPosition, wxDefaultSize, 0);
+	itemBoxSizer11->Add(itemStaticText12, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+	wxButton* itemButton13 = new wxButton(itemPanel1, ID_BUTTON5, _("Exit"), wxDefaultPosition, wxDefaultSize, 0);
+	itemBoxSizer11->Add(itemButton13, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+
+}
+
+cMainPanel::~cMainPanel()
+{
+
+}
+
+
+void cMainPanel::OnButtonOperator(wxCommandEvent &evt)
+{
+	//cCLFrame* frame = dynamic_cast<cCLFrame*>(wxGetTopLevelParent(this));
+	//if (!frame)
+	//	return;
+
+	//frame->m_mainPanel->Hide();
+	//frame->m_loginPanel->Show();
+	//frame->Layout();
+}
--- ComLaser/MainPanel.h
+++ ComLaser/MainPanel.h
@@ -10,22 +10,22 @@
 #include "wx/wx.h"
 #endif
 
-
-class cMainPanel : public wxPanel
-{
-public:
-	cMainPanel(wxFrame*frame);
-	virtual ~cMainPanel();
-
-
-protected:
-
-
-public:
-	
-
-	// message handling
-protected:
-	DECLARE_EVENT_TABLE()
-	void OnButtonOperator(wxCommandEvent &);
-};
+//
+//class cMainPanel : public wxPanel
+//{
+//public:
+//	cMainPanel(wxFrame*frame);
+//	virtual ~cMainPanel();
+//
+//
+//protected:
+//
+//
+//public:
+//	
+//
+//	// message handling
+//protected:
+//	DECLARE_EVENT_TABLE()
+//	void OnButtonOperator(wxCommandEvent &);
+//};
